abdea63c3a6f6c7162c3affac9fadbca4eb47899
Atul1901 <kumar.atul.20299@gmail.com>
2024-02-15 18:43:38 +0530

    completed the card section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import a from "./assets/react-core-concepts.png";
+import reactImg from "./assets/components.png";
+import JSXimg from "./assets/jsx-ui.png";
+import PropsImg from "./assets/config.png";
+import StateImg from "./assets/state-mgmt.png";
+
+const reactDescription = ["Fundamental", "crucial", "core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
 function Header() {
   return (
@@ -9,10 +18,20 @@ function Header() {
       />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescription[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
+  );
+}
+
+function Cards(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -21,6 +40,30 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <ul>
+            <Cards
+              title="Components"
+              description="The core UI building block"
+              img={reactImg}
+            />
+            <Cards
+              title="JSX"
+              description="Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered."
+              img={JSXimg}
+            />
+            <Cards
+              title="props"
+              description="Make components configurable (and therefore reusable) by passing input data to them."
+              img={PropsImg}
+            />
+            <Cards
+              title="State"
+              description="React-managed data which, when changed, causes the component to re-render & the UI to update."
+              img={StateImg}
+            />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
