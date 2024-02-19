c25139fdb28a72c551b5b89bfd04fbeeaff4a150
Atul1901 <kumar.atul.20299@gmail.com>
2024-02-19 13:19:51 +0530

    final completion after deviding the sections into components

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@
 // import PropsImg from "./assets/config.png";
 // import StateImg from "./assets/state-mgmt.png";
 import { Header } from "./components/Header.js";
-import { Cards } from "./components/Cards.js";
-import { TabButton } from "./components/TabButton.js";
-import { useState } from "react";
-import { CORE_CONCEPTS, EXAMPLES } from "./components/data.js";
+// import { TabButton } from "./components/TabButton.js";
+// import { useState } from "react";
+// import { EXAMPLES } from "./components/data.js";
+import { Cardfeature } from "./components/Cards-feature.jsx";
+import { Examples } from "./components/Examples.jsx";
 
 // const reactDescription = ["Fundamental", "crucial", "core"];
 
@@ -40,99 +41,30 @@ import { CORE_CONCEPTS, EXAMPLES } from "./components/data.js";
 // }
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  // const [selectedTopic, setSelectedTopic] = useState("components");
 
-  const handleSelect = (selectedButton) => {
-    setSelectedTopic(selectedButton);
-  };
-
-  console.log("selectedTopic >> ", selectedTopic);
+  // const handleSelect = (selectedButton) => {
+  //   setSelectedTopic(selectedButton);
+  // };
 
   // console.log(
   //   "{EXAMPLES[selectedTopic].title} > ",
   //   EXAMPLES[selectedTopic]?.title
   // );
 
-  console.log("EXAMPLES > ", EXAMPLES);
+  // console.log("EXAMPLES > ", EXAMPLES);
 
-  console.log("EXAMPLES[selectedTopic] > ", EXAMPLES[selectedTopic]);
+  // console.log("EXAMPLES[selectedTopic] > ", EXAMPLES[selectedTopic]);
 
   return (
-    <div>
+    <>
       <Header />
+
       <main>
-        <section id="core-concepts">
-          <ul>
-            {/* <Cards
-              title="Components"
-              description="The core UI building block"
-              img={reactImg}
-            />
-            <Cards
-              title="JSX"
-              description="Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered."
-              img={JSXimg}
-            />
-            <Cards
-              title="props"
-              description="Make components configurable (and therefore reusable) by passing input data to them."
-              img={PropsImg}
-            />
-            <Cards
-              title="State"
-              description="React-managed data which, when changed, causes the component to re-render & the UI to update."
-              img={StateImg}
-            /> */}
-            {CORE_CONCEPTS.map((CardItem, index) => (
-              <Cards key={index} {...CardItem}></Cards>
-            ))}
-
-            {/* <Cards pro {CORE_CONCEPTS[0]}></Cards> */}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-
-          {!selectedTopic ? (
-            <p>please select a topic</p>
-          ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic]?.title}</h3>
-              <p>{EXAMPLES[selectedTopic]?.description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic]?.code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <Cardfeature></Cardfeature>
+        <Examples></Examples>
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Examples.jsx
@@ -0,0 +1,55 @@
+import { TabButton } from "./TabButton.js";
+import { EXAMPLES } from "./data.js";
+import { useState } from "react";
+import { Section } from "./Sections.jsx";
+
+export function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  const handleSelect = (selectedButton) => {
+    setSelectedTopic(selectedButton);
+  };
+  return (
+    <Section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onSelect={() => handleSelect("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onSelect={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onSelect={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onSelect={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+
+      {!selectedTopic ? (
+        <p>please select a topic</p>
+      ) : (
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic]?.title}</h3>
+          <p>{EXAMPLES[selectedTopic]?.description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic]?.code}</code>
+          </pre>
+        </div>
+      )}
+    </Section>
+  );
+}
